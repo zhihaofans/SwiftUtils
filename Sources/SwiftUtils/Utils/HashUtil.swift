8db0ff47a45de480e6f289a5bb30394a44833e61
zhihaofans <zhihaofans@hotmail.com>
2024-06-22 23:57:37 +0800

    add sha256, sha512, sha384

--- Sources/SwiftUtils/Utils/HashUtil.swift
+++ Sources/SwiftUtils/Utils/HashUtil.swift
@@ -5,19 +5,26 @@
 //  Created by zzh on 2024/6/22.
 //
 
-import CommonCrypto // 引入你的 C 包装模块
+import CryptoKit // 引入你的 C 包装模块
 import Foundation
 
 class HashUtil {
     public init() {}
-    public func sha256(_ string: String) -> String {
-        guard let data = string.data(using: .utf8) else { return "" }
-        var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+    public func sha256(input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        return hashedData.compactMap { String(format: "%02x", $0) }.joined()
+    }
 
-        data.withUnsafeBytes {
-            _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &hash)
-        }
+    public func sha512(input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA512.hash(data: inputData)
+        return hashedData.compactMap { String(format: "%02x", $0) }.joined()
+    }
 
-        return hash.map { String(format: "%02x", $0) }.joined()
+    public func sha384(input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA384.hash(data: inputData)
+        return hashedData.compactMap { String(format: "%02x", $0) }.joined()
     }
 }
