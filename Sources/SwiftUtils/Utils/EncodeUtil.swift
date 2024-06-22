2ec80452410eb2ea922bc53b2b638da0c4040ee7
zhihaofans <zhihaofans@hotmail.com>
2024-06-22 23:57:11 +0800

    add EncodeUtil.base64ImageEncode

--- Sources/SwiftUtils/Utils/EncodeUtil.swift
+++ Sources/SwiftUtils/Utils/EncodeUtil.swift
@@ -29,4 +29,13 @@ public class EncodeUtil {
         }
         return String(data: data, encoding: .utf8) ?? oldString
     }
+
+    public func base64ImageEncode(imageData: Data) -> String {
+        // 将图片转换为 JPEG 格式的 Data (也可以选择转换为 PNG 格式)
+        /* guard let imageData = image.jpegData(compressionQuality: 1.0) else {
+             return nil
+         } */
+        // 将 Data 转换为 base64 编码的字符串
+        return imageData.base64EncodedString()
+    }
 }
