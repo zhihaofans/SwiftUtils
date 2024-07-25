4517e059610412a10b78a941669db7c7dfb2539b
zhihaofans <zhihaofans@hotmail.com>
2024-07-25 09:01:14 +0800

    add getDeviceName and getDeviceModel for ios

--- Sources/SwiftUtils/Utils/DeviceUtil.swift
+++ Sources/SwiftUtils/Utils/DeviceUtil.swift
@@ -6,7 +6,12 @@
 //
 
 import Foundation
-
+#if canImport(UIKit)
+import UIKit
+#endif
+#if canImport(AppKit)
+import AppKit
+#endif
 public class DeviceUtil {
     public init() {}
 
@@ -23,6 +28,37 @@ public class DeviceUtil {
         return true
         #else
         return false
+        #endif
+    }
+
+    public func getSystemVersion() -> String {
+        #if os(macOS)
+        let processInfo = ProcessInfo.processInfo
+        let osVersion = processInfo.operatingSystemVersion
+        let versionString = "macOS \(osVersion.majorVersion).\(osVersion.minorVersion).\(osVersion.patchVersion)"
+        return versionString
+        #else
+        return UIDevice.current.systemVersion
+        #endif
+    }
+
+//    let device = UIDevice.current
+//    print("Device name: \(device.name)")
+//    print("Device model: \(device.model)") // 如 "iPhone", "iPod touch"
+//    print("System name: \(device.systemName)")
+//    print("System version: \(device.systemVersion)")
+    public func getDeviceName() -> String {
+        #if os(macOS)
+        return "macOS"
+        #else
+        return UIDevice.current.name
+        #endif
+    }
+    public func getDeviceModel() -> String {
+        #if os(macOS)
+        return "macOS"
+        #else
+        return UIDevice.current.isBatteryMonitoringEnabled
         #endif
     }
 }
