52b08079efb2ae6449cbfd5cb86431b4a1f73c45
zhihaofans <zhihaofans@hotmail.com>
2024-08-13 00:26:51 +0800

    add String.getString

--- Sources/SwiftUtils/Extensions/StringEx.swift
+++ Sources/SwiftUtils/Extensions/StringEx.swift
@@ -15,13 +15,24 @@ public extension String {
     var isInt: Bool {
         return Int(self) != nil
     }
-    func has(keyword:String) -> Bool {
+
+    func has(keyword: String) -> Bool {
         return self.contains(keyword)
     }
-    func replace(of:String, with:String) -> String {
-        return self.replacingOccurrences(of:of,with:with)
+
+    func replace(of: String, with: String) -> String {
+        return self.replacingOccurrences(of: of, with: with)
+    }
+
+    func getString(defaultValue: String) -> String {
+        if self.count == 0 {
+            return defaultValue
+        } else {
+            return self
+        }
     }
 }
+
 public extension String? {
     var isNotEmpty: Bool {
         return (self ?? "").isEmpty == false
@@ -30,6 +41,7 @@ public extension String? {
     var isInt: Bool {
         return Int(self ?? "") != nil
     }
+
     func getString(defaultValue: String) -> String {
         return self ?? defaultValue
     }
