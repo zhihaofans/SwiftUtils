bdbd3e9fe227cd5af0c6ec3cb641b20d8d243a35
zhihaofans <zhihaofans@hotmail.com>
2024-09-01 22:33:33 +0800

    update UserDefaultUtil

--- Sources/SwiftUtils/Utils/UserDefaultUtil.swift
+++ Sources/SwiftUtils/Utils/UserDefaultUtil.swift
@@ -9,8 +9,10 @@ import Foundation
 
 public class UserDefaultUtil {
     private let userDefault = UserDefaults()
-    public init() {
-//        self.userDefault.object(forKey: <#T##String#>)
+    public init() {}
+
+    public func hasValue(key: String) -> Bool {
+        return self.userDefault.object(forKey: key) != nil
     }
 
     public func remove(key: String) {
@@ -21,17 +23,28 @@ public class UserDefaultUtil {
         self.userDefault.setValue(value, forKey: key)
     }
 
-    public func getString(key: String, defaultValue: String?) -> String? {
-        let result = self.userDefault.value(forKey: key) as? String
-        return result ?? defaultValue
+    public func getString(key: String) -> String? {
+        return self.userDefault.string(forKey: key)
+    }
+
+    public func getString(key: String, defaultValue: String) -> String {
+        return self.userDefault.string(forKey: key) ?? defaultValue
     }
 
     public func setInt(key: String, value: Int) {
         self.userDefault.setValue(value, forKey: key)
     }
 
-    public func getInt(key: String, defaultValue: Int?) -> Int? {
-        return self.userDefault.value(forKey: key) as? Int ?? defaultValue
+    public func getInt(key: String) -> Int? {
+        return self.userDefault.integer(forKey: key)
+    }
+
+    public func setFloat(key: String, value: Float) {
+        self.userDefault.setValue(value, forKey: key)
+    }
+
+    public func getFloat(key: String) -> Float? {
+        return self.userDefault.float(forKey: key)
     }
 
     public func setArrayString(key: String, value: [String]) {
@@ -50,11 +63,7 @@ public class UserDefaultUtil {
         self.userDefault.setValue(value, forKey: key)
     }
 
-    public func getBool(key: String, defaultValue: Bool?) -> Bool? {
-        return self.userDefault.value(forKey: key) as? Bool ?? defaultValue
-    }
-
-    public func hasValue(key: String) -> Bool {
-        return self.userDefault.object(forKey: key) != nil
+    public func getBool(key: String) -> Bool? {
+        return self.userDefault.bool(forKey: key)
     }
 }
