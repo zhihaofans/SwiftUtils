fc4dc43a3bbf81544769366c92735787088ab575
zhihaofans <zhihaofans@hotmail.com>
2024-10-25 22:34:06 +0800

    test

---
+++ Sources/SwiftUtils/UI/ListUI.swift
@@ -0,0 +1,32 @@
+//
+//  ListUI.swift
+//
+//
+//  Created by zzh on 2024/10/25.
+//
+
+import SwiftUI
+
+struct ListItemLoadingView: View {
+    var title: String
+    @Binding var isLoading: Bool
+    var onClick: () -> Void
+    var loadingColor: Color? = nil
+    var body: some View {
+        Button(action: {
+            if !isLoading {
+                isLoading = true
+                onClick()
+            }
+        }) {
+            HStack {
+                Text(title)
+                // Spacer() // 占据剩余空间，将 ProgressView 推到右侧
+                if isLoading {
+                    ProgressView()
+                    .tint(loadingColor)
+                }
+            }
+        }
+    }
+}
