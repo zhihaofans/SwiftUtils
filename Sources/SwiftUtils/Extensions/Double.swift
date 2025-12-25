f8883178d5d44ba011ebaedb127d51781be1d7e6
zhihaofans <zhihaofans@hotmail.com>
2025-12-26 00:37:25 +0800

    add Double.toString

---
+++ Sources/SwiftUtils/Extensions/Double.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  SwiftUtils
+//
+//  Created by zzh on 2025/12/26.
+//
+
+import Foundation
+
+public extension Double {
+    var toString: String {
+        return String(self)
+    }
+}
